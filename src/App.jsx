c686d92bc21581008e0d5ca67dbde8e57bc5744c
Natascha <nataschaschlegel27@gmail.com>
2024-01-14 13:30:02 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
