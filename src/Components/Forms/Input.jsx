959cd29c6ee6ec02a452319363bcdbdd110696ca
Natascha <nataschaschlegel27@gmail.com>
2024-01-24 11:21:00 +0100

    hook useForm added

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name}>{label}</label>
-      <input id={name} name={name} className={styles.input} type={type} />
+      <input id={name} name={name} className={styles.input} type={type} value={value} onChange={onChange} />
       <p className={styles.error}>Error</p>
     </div>
   );
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+
+  const username = useForm();
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -13,7 +15,7 @@ const LoginForm = () => {
     fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ username, password }),
     })
@@ -30,8 +32,8 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label='Usuário' type='text' name='username' />
-        <Input label='Senha' type='password' name='password' />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button disabled>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
