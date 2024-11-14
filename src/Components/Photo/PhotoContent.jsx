052d81740398343593dbcd3d8b7b3c0ad5102778
Natascha <46815651+natSchlegel@users.noreply.github.com>
2024-11-14 21:57:34 +0100

    Updates

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./PhotoContent.module.css";
 import { Link } from "react-router-dom";
+import PhotoComments from "./PhotoComments";
 
 const PhotoContent = ({ data }) => {
   const { photo, comments } = data;
@@ -18,8 +19,13 @@ const PhotoContent = ({ data }) => {
           <h1 className="title">
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
           </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
         </div>
       </div>
+      <PhotoComments id={photo.id} comments={comments} />
     </div>
   );
 };
